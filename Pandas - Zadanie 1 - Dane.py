9103482918c4e7c5d09f70e946879c2df5b3de5c
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2022-08-10 20:26:13 +0000

    Update Pandas - Zadanie 1 - Dane.py - uzupełnienie nazw kolumn, połączenie z plikiem CSV, który nie zawierał nazw kolumn

--- Pandas - Zadanie 1 - Dane.py
+++ Pandas - Zadanie 1 - Dane.py
@@ -15,6 +15,22 @@
 
 import pandas as pd
 
+colnames=['imię', 
+          'nazwisko', 
+          'rok urodzenia', 
+          'miesiac urodzenia',
+          'dzień urodzenia', 
+          'miejsce urodzenia',
+          'miasto, w którym znajduje się oddział firmy',
+          'departament, w którym pracuje dana osoba',
+          'rok roczęcia pracy w firmie',
+          'miesiac rozpoczęcia pracy',
+          'dzień rozpoczęcia pracy',
+          'doswiadczenie (w latach)']
+          
 # Wczytywanie pliku CSV
-df = pd.read_csv('dataset_pandas_pd.csv', delimiter=',')
+df = pd.read_csv('dataset_pandas_pd.csv', 
+                 delimiter=',', 
+                 names=colnames)
+                     
 df
