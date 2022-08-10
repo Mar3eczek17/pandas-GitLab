e494cee30e66c80b7cb45e937c6bef2c5d026ad7
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2022-08-10 19:57:01 +0000

    Update Pandas - Zadanie 1 - Dane.py - poprawa wczytywania pliku CSV

--- Pandas - Zadanie 1 - Dane.py
+++ Pandas - Zadanie 1 - Dane.py
@@ -15,17 +15,6 @@
 
 import pandas as pd
 
-# Nowy DataFrame
-df = pd.DataFrame({'imię': [], 
-                   'nazwisko': [], 
-                   'rok urodzenia': [], 
-                   'miesiac urodzenia': [], 
-                   'dzień urodzenia': [], 
-                   'miejsce urodzenia': [], 
-                   'miasto, w którym znajduje się oddział firmy': [],
-                   'departament, w którym pracuje dana osoba': [],
-                   'rok roczęcia pracy w firmie': [], 
-                   'miesiac rozpoczęcia pracy': [], 
-                   'dzień rozpoczęcia pracy': [], 
-                   'doswiadczenie (w latach)': []})
+# Wczytywanie pliku CSV
+df = pd.read_csv('dataset_pandas_pd.csv', delimiter=',')
 df
