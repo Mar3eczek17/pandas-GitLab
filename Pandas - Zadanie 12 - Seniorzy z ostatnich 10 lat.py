9377e995c4c1ba270c5e319fa448f798773b4220
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2022-08-15 11:38:19 +0000

    Add new file Pandas - Zadanie 12 - Seniorzy z ostatnich 10 lat.py

---
+++ Pandas - Zadanie 12 - Seniorzy z ostatnich 10 lat.py
@@ -0,0 +1,42 @@
+# Pandas - Zadanie 12 - Seniorzy z ostatnich 10 lat
+# Ile osób, które posiadają obecnie stanowisko seniorskie, dołączyło do firmy w przeciągu ostatnich 10 lat?
+import pandas as pd
+
+colnames=['imię', 
+          'nazwisko', 
+          'rok_urodzenia', 
+          'miesiac_rodzenia',
+          'dzień_rodzenia', 
+          'miejsce_rodzenia',
+          'miasto_w_którym_znajduje_się_oddział_firmy',
+          'departament_w_którym_pracuje_dana_osoba',
+          'rok_roczęcia_pracy_w_firmie',
+          'miesiac_rozpoczęcia_pracy',
+          'dzień_rozpoczęcia_pracy',
+          'doswiadczenie_w_latach']
+          
+# Wczytywanie pliku CSV
+df = pd.read_csv('dataset_pandas_pd.csv', 
+                 delimiter=',', 
+                 names=colnames)
+
+# Do zbioru danych należy dodać kolumnę, która będzie mówiła o poziomie doświadczenia danej osoby, w sposób
+# słowny, przyjmując:
+# - Junior (do 3 lat doświadczenia)
+# - Mid (4 do 8 lat)
+# - Senior (9 do 12 lat)
+# - Ekspert (od 13 lat)
+import numpy as np
+from numpy import nan
+Conditions = [(df['doswiadczenie_w_latach'] <= 3),
+              (4 <= df['doswiadczenie_w_latach']) & (df['doswiadczenie_w_latach'] <= 8),
+             (9 <= df['doswiadczenie_w_latach']) & (df['doswiadczenie_w_latach'] <= 12),
+             (13 <= df['doswiadczenie_w_latach'])]
+Categories = ['Junior','Mid','Senior','Ekspert']
+df['poziom_doświadczenia']=np.select(Conditions, Categories)
+
+# Pandas - Zadanie 12 - Seniorzy z ostatnich 10 lat
+# Ile osób, które posiadają obecnie stanowisko seniorskie, dołączyło do firmy w przeciągu ostatnich 10 lat?
+dast = df.rok_roczęcia_pracy_w_firmie.max()
+dats = df[(df['poziom_doświadczenia'] == 'Senior') & (df['rok_roczęcia_pracy_w_firmie'] > (dast-10))].count().imię
+dats
